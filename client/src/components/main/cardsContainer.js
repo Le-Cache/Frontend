9a69390afbb845e5de024335b06942569885080d
Eileen Cuevas <eileencuevas62@gmail.com>
2020-01-03 03:08:09 -0500

    adds routing, about page
    Co-authored-by: robpedersendev <40696979+robpedersendev@users.noreply.github.com>
    Co-authored-by: informagician <6990987+informagician@users.noreply.github.com>
    Co-authored-by: JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
    Co-authored-by: daisymesa <42687274+daisymesa@users.noreply.github.com>

--- client/src/components/main/cardsContainer.js
+++ client/src/components/main/cardsContainer.js
@@ -6,17 +6,76 @@ const CardsContainer = props => {
         return (
             <main>
                 <div className="top">
-                    <p><span>&#x2717; Dummy Tag</span></p>
+                    {props.filters.includes('Android Development') ? (
+                        <span>&#x2717; Android Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('Data Science') ? (
+                        <span>&#x2717; Data Science</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('iOS Development') ? (
+                        <span>&#x2717; iOS Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('Web Development') ? (
+                        <span>&#x2717; Web Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    <div
+                        className={
+                            props.filters.includes('UX Design')
+                                ? ''
+                                : 'inactive'
+                        }
+                    >
+                        &#x2717; UX Design
+                    </div>
                 </div>
                 <div className="cards">
-                {props.users.map((user, index) => (
-                    <UserCard key={index} data={user.fields} />
-                ))}
+                    {props.users.map((user, index) => (
+                        <UserCard key={index} data={user.fields} />
+                    ))}
                 </div>
             </main>
         );
     } else {
-        return <div>Loading...</div>;
+        return (
+            <main>
+                <div className="top">
+                    {props.filters.includes('Android Development') ? (
+                        <span>&#x2717; Android Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('Data Science') ? (
+                        <span>&#x2717; Data Science</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('iOS Development') ? (
+                        <span>&#x2717; iOS Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('Web Development') ? (
+                        <span>&#x2717; Web Developer</span>
+                    ) : (
+                        <></>
+                    )}
+                    {props.filters.includes('UX Design') ? (
+                        <span>&#x2717; UX Design</span>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+                <div className="cards"></div>
+            </main>
+        );
     }
 };
 
